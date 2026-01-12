e4c16ca35c21f57a6e62e7ac212529afda8a1e66
Vrollo <git@morinoyama.nl>
2026-01-12 17:37:29 +0100

    Gameloop + added clock object to limit fps

--- main.py
+++ main.py
@@ -7,8 +7,13 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    # Initialize Pygame
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # Setup a clock object to be used for FPS calculation/limitation
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         log_state()
@@ -18,6 +23,8 @@ def main():
 
         screen.fill((0, 0, 0))
         pygame.display.flip()
+        # FPS functions
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
